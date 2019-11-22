1621163d47501c05bdc5cbfb50fb0e809b367bca
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-21 22:19:10 -0500

    remove rubocop references

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rubocop_linter_action_playground_production"
+  # config.active_job.queue_name_prefix = "haml_lint_action_playground_production"
 
   config.action_mailer.perform_caching = false
 
